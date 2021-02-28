ddeacf1dd6f310779b89ae49bd56f8e6c7cb22be
Rahmi Balık <rahmi.balik@arvato.com>
2021-02-28 20:19:08 +0300

    20.19

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public bool Success { get; }
+
+        public string Message { get; }
+
+        public Result(bool success, string message=null)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
